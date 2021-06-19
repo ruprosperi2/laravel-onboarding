b5aa7f2764525864595cf04cce1f4e8bef3c8d63
ruben arias <rubenjariasp@gmail.com>
2021-06-19 16:45:11 -0400

    creando el primer seeder para la tabla invoices

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Invoice;
 use Illuminate\Database\Seeder;
 
 class InvoiceSeeder extends Seeder
@@ -13,6 +14,15 @@ class InvoiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $invoice = new Invoice;
+
+        $invoice->supplier = 'Empresas Polar';
+        $invoice->pay_term = 'Pagar antes de que finalice el mes';
+        $invoice->date = '2021-06-12';
+        $invoice->created = 'Ruben';
+        $invoice->status = 'C';
+        $invoice->observations = 'Sin novedad';
+
+        $invoice->save();
     }
 }
