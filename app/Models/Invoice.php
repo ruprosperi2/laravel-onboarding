42971ce6aed0fbe9764930a598e483075af2d9ad
ruben arias <rubenjariasp@gmail.com>
2021-06-19 16:36:07 -0400

    modificando propiedad fillable del modelo Invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'supplier',
+        'pay_term',
+        'date',
+        'created',
+        'status',
+        'observations'
+    ];
 }
