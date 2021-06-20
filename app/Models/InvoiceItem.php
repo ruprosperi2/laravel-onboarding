aaea67af0b8d54d52c6398eb677f0f774bd719c8
ruben arias <rubenjariasp@gmail.com>
2021-06-20 16:14:15 -0400

    se modifico la propiedad filable del modelo de InvoiceItem

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'amount',
+        'price',
+        'subtotal',
+        'invoice_id',
+        'invoice_id'
+    ];
 }
