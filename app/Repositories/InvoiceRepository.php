871b756f24becdc57367af61e26877fb78aea765
ruprosperi2 <rubenjariasp@gmail.com>
2021-07-21 13:59:05 -0400

    refactorizando metodo read del repositorio

--- app/Repositories/InvoiceRepository.php
+++ app/Repositories/InvoiceRepository.php
@@ -2,15 +2,27 @@
 namespace App\Repositories;
 
 use App\Repositories\Interfaces\BaseRepositoryInterface;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
 
 class InvoiceRepository implements BaseRepositoryInterface
 {
+    private $invoice;
+    private $invoiceItem;
+
+    public function __construct(Invoice $invoice, InvoiceItem $invoiceItem)
+    {
+        $this->invoice = $invoice;
+        $this->invoiceItem = $invoiceItem;
+    }
+
     public function create(array $data){
         return "create";
     }
 
     public function read(){
-        return "read";
+        return $this->invoice->all()->toJson();
+        //return Invoice::all()->toJson();
     }
 
     public function update(array $data, $id){
