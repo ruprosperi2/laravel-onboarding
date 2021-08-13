c0502f828c8ce25ac29ae3f7ad7da69e3b5264a5
ruprosperi2 <rubenjariasp@gmail.com>
2021-08-12 23:08:46 -0400

    probando el find

---
+++ src/PurchaseInvoice/Domain/ValueObjects/SubTotal.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Src\PurchaseInvoice\Domain\ValueObjects;
+
+use Shared\Domain\ValueObject\FloatValueObject;
+
+class SubTotal extends FloatValueObject
+{
+
+}
--- src/PurchaseInvoice/Infrastructure/Repository/PurchaseInvoiceMysqlRepository.php
+++ src/PurchaseInvoice/Infrastructure/Repository/PurchaseInvoiceMysqlRepository.php
@@ -6,25 +6,56 @@ use Illuminate\Support\Facades\DB;
 use Src\PurchaseInvoice\Domain\Contract\PurchaseInvoiceRepository;
 use Src\PurchaseInvoice\Domain\ValueObjects\Id;
 use Src\PurchaseInvoice\Domain\PurchaseInvoice;
+use Src\PurchaseInvoice\Domain\ValueObjects\Supplier;
+use Src\PurchaseInvoice\Domain\ValueObjects\Payterm;
+use Src\PurchaseInvoice\Domain\ValueObjects\DateCreation;
+use Src\PurchaseInvoice\Domain\ValueObjects\Created;
+use Src\PurchaseInvoice\Domain\ValueObjects\Status;
+use Src\PurchaseInvoice\Domain\ValueObjects\Observations;
+use Src\PurchaseInvoice\Domain\ValueObjects\Items;
 
 class PurchaseInvoiceMysqlRepository implements PurchaseInvoiceRepository
 {
     public function findAll(): object
     {
         return $result = DB::table('invoices')->get();
-
     }
 
     public function find(Id $id): PurchaseInvoice
     {
-
-        $row = DB::table('invoices')
+        $data = DB::table('invoices')
             ->join('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-            ->select('invoices.supplier', 'invoice_items.name')
+            ->select('invoices.*', 'invoice_items.*')
             ->where('invoices.id', '=', $id->value())
             ->get();
 
-        dd($row);
+       $arreglo = json_decode($data, TRUE);
+
+        $purchaseInvoiceItems = [];
+
+        for($i=0; $i<count($arreglo); $i++){
+            $purchaseInvoiceItems[$i]['name'] = $arreglo[$i]['name'];
+            $purchaseInvoiceItems[$i]['amount'] = $arreglo[$i]['amount'];
+            $purchaseInvoiceItems[$i]['price'] = $arreglo[$i]['price'];
+            $purchaseInvoiceItems[$i]['subtotal'] = $arreglo[$i]['subtotal'];
+            $purchaseInvoiceItems[$i]['invoice_id'] = $arreglo[$i]['invoice_id'];
+        }
+
+        $items = new Items($purchaseInvoiceItems);
+
+        $invoice = [];
+
+        foreach ($arreglo as $item){
+            $invoice['supplier'] =  new Supplier($item['supplier']);
+            $invoice['pay_term'] = new Payterm($item['pay_term']);
+            $invoice['date'] = new DateCreation($item['date']);
+            $invoice['created'] = new Created($item['created']);
+            $invoice['status'] = new Status($item['status']);
+            $invoice['observations'] = new Observations($item['observations']);
+            $invoice['items'] = $items;
+        }
+
+
 
     }
 
